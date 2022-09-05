36efa33ab91f21d74fdfc3f190c57a8c00a34c0d
Shaikat <49788830+shaikat17@users.noreply.github.com>
2022-09-05 23:00:44 +0600

    Create Growth of a Population.py

---
+++ Growth of a Population.py
@@ -0,0 +1,10 @@
+import math
+
+def nb_year(p0, percent, aug, p):
+    year = 0
+    
+    while p0 < p:
+        p0 += math.floor((p0 * percent/100)) + aug 
+        year += 1
+        
+    return year
