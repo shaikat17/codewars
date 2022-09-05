0560b9977974e4a07ad55535ca7c31763c0cd488
Shaikat <49788830+shaikat17@users.noreply.github.com>
2022-09-05 22:27:49 +0600

    Create Welcome!.py

---
+++ Welcome!.py
@@ -0,0 +1,26 @@
+def greet(language):
+    database = {
+        'english': 'Welcome',
+'czech': 'Vitejte',
+'danish': 'Velkomst',
+'dutch': 'Welkom',
+'estonian': 'Tere tulemast',
+'finnish': 'Tervetuloa',
+'flemish': 'Welgekomen',
+'french': 'Bienvenue',
+'german': 'Willkommen',
+'irish': 'Failte',
+'italian': 'Benvenuto',
+'latvian': 'Gaidits',
+'lithuanian': 'Laukiamas',
+'polish': 'Witamy',
+'spanish': 'Bienvenido',
+'swedish': 'Valkommen',
+'welsh': 'Croeso'
+
+    }
+    
+    try:
+        return database[language]
+    except:
+        return 'Welcome'
